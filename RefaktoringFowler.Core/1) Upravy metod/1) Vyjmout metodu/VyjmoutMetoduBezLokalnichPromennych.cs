d24c52d9f66df35f6b7c190f875941578d74d7a6
Martin Okosy <m.okosy@seznam.cz>
2016-03-02 08:37:00 +0100

    Vyjmout metodu - Bez lokalnich promennych - Original

---
+++ RefaktoringFowler.Core/1) Upravy metod/1) Vyjmout metodu/VyjmoutMetoduBezLokalnichPromennych.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefaktoringFowler.Core.VyjmoutMetodu
+{
+    public class VyjmoutMetoduBezLokalnichPromennych
+    {
+        private string _jmeno ;
+        private List<Zakazka> _zakazky;
+        
+        public VyjmoutMetoduBezLokalnichPromennych()
+        {
+            _jmeno = "Karel Vomacka";
+            _zakazky = new List<Zakazka>
+            {
+                new Zakazka(15.25),
+                new Zakazka(18.30),
+                new Zakazka(5.50)
+            };
+        }
+        
+        public void TiskUctu()
+        {
+            double zaplatit = 0.0;
+
+            // tisk zahlavi
+            Console.WriteLine("******************************");
+            Console.WriteLine("***** Zakaznik ma platit *****");
+            Console.WriteLine("******************************");
+
+            // vypocet castky k zaplaceni
+            foreach (var zakazka in _zakazky)
+            {
+                zaplatit += zakazka.Castka;
+            }
+
+            // tisk polozky
+            Console.WriteLine("Jmeno: " + _jmeno);
+            Console.WriteLine("Castka: " + zaplatit);
+        }
+    }
+}
---
+++ RefaktoringFowler.Core/1) Upravy metod/1) Vyjmout metodu/Zakazka.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefaktoringFowler.Core.VyjmoutMetodu
+{
+    public class Zakazka
+    {
+        public double Castka { get; private set; }
+
+        public Zakazka(double castka)
+        {
+            Castka = castka;
+        }
+    }
+}
