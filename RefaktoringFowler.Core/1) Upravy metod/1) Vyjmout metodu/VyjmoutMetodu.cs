c19fa8947e336493b70fa8466e6f5fd155cbdaae
Martin Okosy <m.okosy@seznam.cz>
2016-03-02 08:14:07 +0100

    VyjmoutMetodu - original

---
+++ RefaktoringFowler.Core/1) Upravy metod/1) Vyjmout metodu/VyjmoutMetodu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RefaktoringFowler.Core.VyjmoutMetodu
+{
+    public class VyjmoutMetodu
+    {
+        private string _jmeno;
+
+        public void TiskUctu(double castka)
+        {
+            TiskZahlavi();
+
+            //tisk polozky
+            Console.WriteLine("Jmeno: " + _jmeno);
+            Console.Write("Castka: " + castka);
+        }
+
+        private void TiskZahlavi()
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("***** Zakaznik ma platit *****");
+            Console.WriteLine("******************************");
+        }
+    }
+}
