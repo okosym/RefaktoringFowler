09670c29f50b7edb281079093c87e17eb8d1bf41
Martin Okosy <m.okosy@seznam.cz>
2016-03-05 06:46:32 +0100

    Vlozit metodu - Original

---
+++ RefaktoringFowler.Core/1) Upravy metod/2) Vlozit metodu/VlozitMetodu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefaktoringFowler.Core.Vlozit_metodu
+{
+    public class VlozitMetodu
+    {
+        private int _pocetZpozdeni = 3;
+
+        public int GetHodnoceni()
+        {
+            return (ViceNezPetZpozdeni()) ? 2 : 1;
+        }
+
+        private bool ViceNezPetZpozdeni()
+        {
+            return _pocetZpozdeni > 5;
+        }
+    }
+}
